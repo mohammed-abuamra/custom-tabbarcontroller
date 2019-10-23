bc7b431e48da00d3f4912a27e7fdeb8c37a922cd
Ghazzway <SD_a3@hotmail.com>
2019-10-23 10:35:53 +0300

    Initial Commit

---
+++ corner&shadowTabbar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  corner&shadowTabbar
+//
+//  Created by 📲Ghazzway📱 😎 on 24/02/1441 AH.
+//  Copyright © 1441 📲Ghazzway📱 😎. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ corner&shadowTabbar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  corner&shadowTabbar
+//
+//  Created by 📲Ghazzway📱 😎 on 24/02/1441 AH.
+//  Copyright © 1441 📲Ghazzway📱 😎. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
